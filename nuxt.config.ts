1f513548c520fdb0495f87da48303209dd68ec20
Evaldas Būzas (valantic) <evaldas.buzas@cec.valantic.com>
2025-03-13 22:14:06 +0200

    asd

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,20 +2,10 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   ssr: false,
-  devtools: { enabled: false },
-  modules: [
-    '@nuxt/eslint',
-    '@nuxt/image',
-    '@nuxt/ui-pro',
-    '@nuxthub/core'
-  ],
+  devtools: { enabled: true },
+  modules: ['@nuxt/eslint', '@nuxt/image', '@nuxt/ui-pro'],
   css: ['~/assets/css/main.css'],
   runtimeConfig: {
     posthogApiKey: process.env.POSTHOG_API_KEY,
   },
-  nitro: {
-    prerender: {
-      autoSubfolderIndex: false
-    }
-  }
 })
