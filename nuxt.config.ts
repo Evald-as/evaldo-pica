1db50b0c1d3685b93e8981b35948a2bde316cc71
Evaldas Būzas (valantic) <evaldas.buzas@cec.valantic.com>
2025-03-13 22:23:36 +0200

    asd

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
-  ssr: false,
   devtools: { enabled: true },
   modules: ['@nuxt/eslint', '@nuxt/image', '@nuxt/ui-pro'],
   css: ['~/assets/css/main.css'],
